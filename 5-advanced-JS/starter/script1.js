4074b298a38da5c8fd59cac9d177e1dd4c55531b
hyangloyaltycom <hyang2@loyalty.com>
2019-12-31 20:45:09 -0500

    closure example added

--- 5-advanced-JS/starter/script1.js
+++ 5-advanced-JS/starter/script1.js
@@ -224,6 +224,22 @@ retirementIceland(1990);
 
 //retirement(66)(1990);
 
+function makeAdder(x) {
+    return function(y) {
+        return x + y;
+    };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+console.log('makeAdder is a function factory — it creates functions which can add a specific value, In the above example we use our function factory to create two new functions — one that adds 5 to its argument, and one that adds 10. add5 and add10 are both closures. They share the same function body definition, but store different lexical environments. In add5\'s lexical environment, x is 5, while in the lexical environment for add10, x is 10');
+console.log(add5(2));  // 7
+console.log(add10(2)); // 12
+/*
+In essence, makeAdder is a function factory — it creates functions which can add a specific value to their argument. In the above example we use our function factory to create two new functions — one that adds 5 to its argument, and one that adds 10.
+
+add5 and add10 are both closures. They share the same function body definition, but store different lexical environments. In add5's lexical environment, x is 5, while in the lexical environment for add10, x is 10.
+*/
 
 function interviewQuestion(job) {
     return function(name) {
